e546e03ec6b5ddede633f11cc9b070ba8a516b5c
Avishkar Deogharia <avishkardeogharia2004@gmail.com>
2025-05-13 03:57:42 +0530

    Final push

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const connectDB = require('./lib/db.js');
 const {app, server } = require('./lib/socket.js');
 
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+
 
 dotenv.config()
 
